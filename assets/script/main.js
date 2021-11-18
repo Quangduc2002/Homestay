8aeda3bd03bfd6e26272ee0d515c81e5d3d04dd6
duc <89673332+Quangduc2002@users.noreply.github.com>
2021-11-18 22:43:45 +0700

    upload

--- assets/script/main.js
+++ assets/script/main.js
@@ -143,3 +143,22 @@
         });
     });
 
+    $(document).ready(function(){
+        $('.english').slick({
+            slidesToShow: 3,
+            infinite: false,
+            prevArrow: '<div  class="slick-prev  fas-left fas-left-TA"><i class="fas fa-angle-left" aria-hidden="true"></i></div>',
+            nextArrow: '<div  class="slick-next fas-right fas-right-TA"><i class="fas fa-angle-right" aria-hidden="true"></i></div>',
+            responsive: [
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1, //di chuyển nhiều ảnh 1 lúc 
+                    infinite: true,
+                  }
+                }
+            ]
+        });
+    });
+
